7ddffaf019514bb3b9f633d34eb47128257d9979
kubat <kubat.aiupov@gmail.com>
2023-04-17 14:39:02 +0200

    fixed bgcolor and text state for mode toggle

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 // Dark and Light Mode Btns
 const darkBtn = document.getElementById("dark");
-const lightBtn = document.getElementById("light");
 const bodyEl = document.querySelector("body");
 
 // Check if dark mode is enabled
 const isDarkMode = localStorage.getItem("darkMode") === "true";
 if (isDarkMode) {
   bodyEl.classList.add("dark");
+  darkBtn.textContent = "Light Mode";
+  darkBtn.style.backgroundColor = "#fff";
+  darkBtn.style.color = "#292c35";
 }
 
 // Toggle between light and dark mode
@@ -14,9 +16,17 @@ darkBtn.addEventListener("click", () => {
   const isCurrentlyDark = bodyEl.classList.contains("dark");
   if (isCurrentlyDark) {
     bodyEl.classList.remove("dark");
+    darkBtn.innerText = "Dark Mode";
     localStorage.setItem("darkMode", "false");
+    darkBtn.textContent = "Dark Mode";
+    darkBtn.style.backgroundColor = "#292c35";
+    darkBtn.style.color = "#fff";
   } else {
     bodyEl.classList.add("dark");
+    darkBtn.innerText = "Light Mode";
     localStorage.setItem("darkMode", "true");
+    darkBtn.textContent = "Light Mode";
+    darkBtn.style.backgroundColor = "#fff";
+    darkBtn.style.color = "#292c35";
   }
 });
