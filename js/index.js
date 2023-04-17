edba37c204d9547406e75b2acdc4025f4fdae90e
kubat <kubat.aiupov@gmail.com>
2023-04-17 13:52:47 +0200

    edit dark mode, to keep mode persistently for all pages

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
-
 // Dark and Light Mode Btns
 const darkBtn = document.getElementById("dark");
 const lightBtn = document.getElementById("light");
 const bodyEl = document.querySelector("body");
 
-darkBtn.addEventListener("click", darkMode);
-lightBtn.addEventListener("click", lightMode);
-
-function darkMode() {
+// Check if dark mode is enabled
+const isDarkMode = localStorage.getItem("darkMode") === "true";
+if (isDarkMode) {
   bodyEl.classList.add("dark");
 }
 
-function lightMode() {
-  bodyEl.classList.remove("dark");
-}
-
+// Toggle between light and dark mode
+darkBtn.addEventListener("click", () => {
+  const isCurrentlyDark = bodyEl.classList.contains("dark");
+  if (isCurrentlyDark) {
+    bodyEl.classList.remove("dark");
+    localStorage.setItem("darkMode", "false");
+  } else {
+    bodyEl.classList.add("dark");
+    localStorage.setItem("darkMode", "true");
+  }
+});
